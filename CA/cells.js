96c09eee03b9854efa3e1dfdf3d8928deeb24fd5
iammmai <dtmai@gmx.de>
2017-12-02 22:16:50 +0100

    Full screen no borders

--- CA/cells.js
+++ CA/cells.js
@@ -1,9 +1,19 @@
 function CellSystem() {
-  this.cells = [];
+  this.cells = new Array(500);
   this.ruleset = [];
   this.generation = 0;
   this.w =3
+  this.history = []
 
+}
+
+CellSystem.prototype.restart = function () {
+    background(0)
+    for (let i =0; i< this.cells.length; i++) {
+        this.cells[i] = Math.floor(random(2))
+    }
+    this.generation = 0
+    c.generateRuleset()
 }
 
 CellSystem.prototype.generateRuleset = function () {
@@ -14,12 +24,22 @@ CellSystem.prototype.generateRuleset = function () {
 
 CellSystem.prototype.generate = function() {
   let newcells = new Array(this.cells.length);
-  for (let i = 1; i < this.cells.length-1; i++) {
-    let newstate = this.rule(this.cells[i - 1], this.cells[i], this.cells[i + 1]);
+  for (let i = 0; i < this.cells.length; i++) {
+      let newstate
+      if(i==0) {
+        newstate = this.rule(this.cells[this.cells.length -1], this.cells[i], this.cells[i + 1]);
+      } else if (i == this.cells.length -1) {
+        newstate = this.rule(this.cells[i - 1], this.cells[i], this.cells[0]);
+        
+      } else {
+        newstate = this.rule(this.cells[i - 1], this.cells[i], this.cells[i + 1]);
+      }  
+
     newcells[i] = newstate;
   }
   this.cells = newcells;
   this.generation += 1;
+  
 };
 
 CellSystem.prototype.rule = function(left, middle, right) {
--- CA/sketch.js
+++ CA/sketch.js
@@ -4,17 +4,16 @@ function setup() {
     createCanvas(windowWidth, windowHeight)
     background(0)
     c= new CellSystem()
-
-    for (let i=0; i<width; i+= c.w) {
-            c.cells.push(Math.floor(random(2)))
-        }
-        
-    c.generateRuleset()
+    c.restart()
+    
 }
 
 function draw() {
-    c.cells[c.cells.length/2] = 1 
+    if(mouseIsPressed) {
+        c.restart()
+    }
     c.render()
     c.generate()
-    
+
 }
+
